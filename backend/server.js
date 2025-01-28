ac20ba3cf3b6b0aad0ffc48a82deb9cf7d096161
Niraj Joshi <infocodewithniraj@gmail.com>
2025-01-28 22:20:58 +0545

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
