5b614340027d86329e2c4a7f8f533d7bc7378cef
Niraj Joshi <infocodewithniraj@gmail.com>
2025-01-28 21:27:24 +0545

    finallll

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -9,7 +8,7 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
+import cors from 'cors';
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -17,10 +16,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Directly setting the URL for the API base URL
-      "/api": {
-        target: "https://ecom-nvp3.onrender.com",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 });
