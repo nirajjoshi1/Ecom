cd11ecd9c6b002f24ee6de1f5c12c56bc06cd670
Niraj Joshi <infocodewithniraj@gmail.com>
2025-01-28 22:00:00 +0545

    aaa

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -17,7 +17,7 @@ const ProductCard = ({ product }) => {
 	};
 
 	return (
-		<div className='flex w-full relative flex-col overflow-hidden rounded-lg shadow-lg'>
+		<div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
 			<div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
 				<img className='object-cover w-full' src={product.image} alt='product image' />
 				<div className='absolute inset-0 bg-black bg-opacity-20' />
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL:  "https://ecom-nvp3.onrender.com/api", 
-	withCredentials: true, // send cookies to the server
+  baseURL: "https://ecom-nvp3.onrender.com/api",
+  withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
